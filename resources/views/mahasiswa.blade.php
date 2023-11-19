957160750679ea9e8ee5f7f9df5922d2ac98ccbe
slayerwitch <55046247+slayerwitch@users.noreply.github.com>
2023-11-19 23:06:40 +0700

    Push project workshop

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Informasi Mahasiswa</h1>
+    <p>Nama : Amro Alfien Syachrian Nadzief</p>
+    <p>Kelas/Prodi : 1A / Teknologi Informasi</p>
+</body>
+</html>
